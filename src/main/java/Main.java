2c30196d5655bc47b82e78e512879fda5db11670
alek <wait82985@gmail.com>
2023-11-13 19:26:58 +0600

    problem solved

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] chars = {'q', 'e', 'E','a', 'b', 'c', 'A'};
+        Arrays.sort(chars);
+        System.out.println(Arrays.toString(chars));
+    }
+}
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class Solution {
     public String sortVowels(String s) {
-        return null;
+        char[] chars = s.toCharArray();
+        List<Integer> indexes = new ArrayList<>();
+        List<Character> characters = new ArrayList<>();
+        Set<Character> vowels = new HashSet<>(List.of('A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'));
+        for (int i = 0; i < chars.length; i++) {
+            if(vowels.contains(chars[i])){
+                indexes.add(i);
+                characters.add(chars[i]);
+            }
+        }
+        Collections.sort(characters);
+        for (int i = 0; i < indexes.size(); i++) {
+            chars[ indexes.get(i)] = characters.get(i);
+        }
+        return String.valueOf(chars);
     }
 }
